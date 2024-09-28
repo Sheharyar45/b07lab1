578c45bd0a0529d6a26b72f9b49cc4ade96d3b25
Sheharyar Meghani <107352319+Sheharyar45@users.noreply.github.com>
2024-09-28 13:11:59 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,11 +5,11 @@ public class Driver {
 	public static void main(String[] args) throws FileNotFoundException  {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double[] c1 = {6,5,-1};
-		int[] e1 = {0,3,2};
+		double[] c1 = {6,5};
+		int[] e1 = {0,3};
 		Polynomial p1 = new Polynomial(c1,e1);
-		double[] c2 = { -6,-5,1};
-		int[] e2 = {1,3,2};
+		double[] c2 = { -2,-9};
+		int[] e2 = {1,4};
 		Polynomial p2 = new Polynomial(c2,e2);
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
@@ -20,7 +20,7 @@ public class Driver {
 		File file = new File("read.txt"); 
 		Polynomial t = new Polynomial(file);
 		Polynomial y = t.multiply(p2);
-		s.saveToFile("file.txt");
+		y.saveToFile("file.txt");
 		
 		
 		
